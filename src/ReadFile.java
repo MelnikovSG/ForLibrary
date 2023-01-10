80885e05a092993cbed1cd43fa06f566607a352a
MelnikovSG <melnikov-sergey-g@mail.ru>
2023-01-10 14:54:12 +0700

    Iteration-3

---
+++ src/ReadFile.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class ReadFile {
+
+    Library library = new Library();
+    public void fileReading() throws IOException {
+
+            BufferedReader reader = new BufferedReader(new FileReader("ListOfBook.txt"));
+            String line;
+            while ((line = reader.readLine()) != null){
+                String[] words = line.split(", ");
+                Book book = new Book();
+                book.setName(words[0]);
+                book.setWriter(words[1]);
+                book.setReleaseYear(words[2]);
+                library.createArray(book);
+            }
+            reader.close();
+    }
+}
